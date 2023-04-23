9b56e9e456b1dc881b639160b0facca2bccc4db9
Vahid Farid <vahidfarid@gmail.com>
2023-04-23 17:29:25 +0330

    Update telegram.ts

--- src/telegram.ts
+++ src/telegram.ts
@@ -1,7 +1,7 @@
 export default class Telegram {
     botUrl: string
 
-    constructor({ token }: {token: string}) {
+    constructor({ token }: { token: string }) {
         this.botUrl = `https://api.telegram.org/bot${token}`
     }
 
@@ -12,6 +12,19 @@ export default class Telegram {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(message)
+        })
+    }
+
+    deleteMessage({ message }: { message: any }): any {
+        return fetch(`${this.botUrl}/deleteMessage`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                chat_id: message.chat.id,
+                message_id: message.message_id,
+            })
         })
     }
 
